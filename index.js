9141c769c8158f223e51b0f2539c01ad4c463dc4
Geof Mureithi <mureithinjuguna@gmail.com>
2017-03-08 21:05:00 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict';
+const BootBot = require('bootbot');
+require('dotenv').config();
+
+const bot = new BootBot({
+    accessToken: process.env.FB_ACCESS_TOKEN,
+    verifyToken: process.env.FB_VERIFY_TOKEN,
+    appSecret: process.env.FB_APP_SECRET
+});
+
+bot.on('message', (payload, chat) => {
+    const text = payload.message.text;
+    chat.say(`Echo: ${text}`);
+    console.log(`The user said: ${text}`);
+});
+
+bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
+    console.log('The user said "hello", "hi", "hey", or "hey there"');
+});
+
+bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
+    // Send a text message followed by another text message that contains a typing indicator
+    chat.say('Hello, human friend!').then(() => {
+        chat.say('How are you today?', { typing: true });
+    });
+});
+
+bot.hear(['food', 'hungry'], (payload, chat) => {
+    // Send a text message with quick replies
+    chat.say({
+        text: 'What do you want to eat today?',
+        quickReplies: ['Mexican', 'Italian', 'American', 'Argentine']
+    });
+});
+
+bot.hear(['help'], (payload, chat) => {
+    // Send a text message with buttons
+    chat.say({
+        text: 'What do you need help with?',
+        buttons: [
+            { type: 'postback', title: 'Settings', payload: 'HELP_SETTINGS' },
+            { type: 'postback', title: 'FAQ', payload: 'HELP_FAQ' },
+            { type: 'postback', title: 'Talk to a human', payload: 'HELP_HUMAN' }
+        ]
+    });
+});
+
+bot.hear('image', (payload, chat) => {
+    // Send an attachment
+    chat.say({
+        attachment: 'image',
+        url: 'http://example.com/image.png'
+    });
+});
+
+
+bot.start();
